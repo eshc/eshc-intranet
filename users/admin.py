a48b01c7ffe94edc68938009cd1e3656045045b2
evcka <filip.kaklin@gmail.com>
2017-05-24 20:50:36 -0500

    Display profile info in User admin table

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import User
 
-# Register your models here.
+from users.models import Profile
+
+admin.site.unregister(User)
+
+class ProfileInLine(admin.StackedInline):
+	model = Profile
+	can_delete = False
+
+class UserProfileAdmin(UserAdmin):
+	inlines = [ ProfileInLine, ]
+
+	def ref_number(self, obj):
+		try:
+			return obj.profile.ref_number
+		except Profile.DoesNotExist:
+			return ''
+
+	list_display = UserAdmin.list_display + ('ref_number', )
+
+admin.site.register(User, UserProfileAdmin)
--- users/models.py
+++ users/models.py
@@ -5,10 +5,19 @@ from django.dispatch import receiver
 
 class Profile(models.Model):
 	user = models.OneToOneField(User, on_delete=models.CASCADE)
-	ref_number = models.CharField(max_length=8, blank=True)
+	ref_number = models.CharField('Bank Reference Number', max_length=8, blank=True)
 	phone_number = models.CharField(max_length=15, blank=True)
 	perm_address = models.TextField(max_length=500, blank=True)
 
+	# full_name = 'string'
+	# full_name = models.
+	# full_name = User.objects.get(username=user)
+	# full_name = User.objects.get(user)
+
+	# def full_name(self):
+	# 	name = 	User.objects.get(id=user.id)
+	# 	return name
+		
 @receiver(post_save, sender=User)
 def create_user_profile(sender, instance, created, **kwargs):
 	if created:
