6c23a5a3cc39ac6b3f140ecc149a7df55bce0c0d
evcka <filip.kaklin@gmail.com>
2017-06-01 13:45:10 -0500

    Change wsgi.py

--- eshcIntranet/wsgi.py
+++ eshcIntranet/wsgi.py
@@ -13,4 +13,8 @@ from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "eshcIntranet.settings")
 
-application = get_wsgi_application()
+from django.core.wsgi import get_wsgi_application
+from whitenoise.django import DjangoWhiteNoise
+
+application = get_wsgi_application() 	# the only original line
+application = DjangoWhiteNoise(application)
