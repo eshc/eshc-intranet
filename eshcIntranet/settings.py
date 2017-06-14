4babc372eff9e72e7d4b968dbeb91700f5fb4dda
evcka <filip.kaklin@gmail.com>
2017-06-14 18:41:16 -0500

    Remove bad import

--- eshcIntranet/settings.py
+++ eshcIntranet/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 
 import os
 import dj_database_url
-import eshcIntranet.gmail_pass as gmail_pass
+# import eshcIntranet.gmail_pass as gmail_pass
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 # BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
