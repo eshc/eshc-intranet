c7bbb3057b9e279be2f7344cc015994761b2495f
evcka <filip.kaklin@gmail.com>
2017-07-25 14:11:37 +0100

    Mode cascade fix for gm

--- home/models.py
+++ home/models.py
@@ -22,14 +22,14 @@ class Point(models.Model):
 	title = models.CharField(max_length=200)
 	description = models.TextField()
 	pub_date = models.DateField('date published')
-	submitted_by = models.ForeignKey(User, on_delete=models.PROTECT)
-	choice = models.ForeignKey(GM, on_delete=models.PROTECT)
+	submitted_by = models.ForeignKey(User, on_delete=models.CASCADE)
+	choice = models.ForeignKey(GM, on_delete=models.CASCADE)
 
 class WgUpdate(models.Model):
 	def __str__(self):
 		return self.text
 
 	text = models.CharField(max_length=500)
-	group = models.ForeignKey(Group, on_delete=models.PROTECT)
-	choice = models.ForeignKey(GM, on_delete=models.PROTECT)
+	group = models.ForeignKey(Group, on_delete=models.CASCADE)
+	choice = models.ForeignKey(GM, on_delete=models.CASCADE)
 	
