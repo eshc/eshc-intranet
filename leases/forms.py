a46d5751a24aaee6a5685f6fa7ed6b754b4fa406
evcka <filip.kaklin@gmail.com>
2017-09-10 19:58:30 +0100

    Fix inventory submission form

--- leases/forms.py
+++ leases/forms.py
@@ -12,7 +12,8 @@ class InventoryForm(forms.ModelForm):
 	def save(self, lease_id, commit=True):
 		inventory = super(InventoryForm, self).save(commit=False)
 		inventory.inventory_notes = self.cleaned_data['inventory_notes']
-		inventory.sub_date = timezone.localdate()
+		# inventory.sub_date = timezone.localdate()		# django 1.11
+		inventory.sub_date = timezone.now().date()		# django 1.10
 		inventory.lease_id = lease_id
 
 		if commit:
