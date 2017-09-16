f6f62968a619250a0aa73030c191bbfd006a1793
evcka <filip.kaklin@gmail.com>
2017-09-16 21:25:09 +0100

    Add form for adding new announcement

--- whiteboard/urls.py
+++ whiteboard/urls.py
@@ -6,4 +6,5 @@ from . import views
 
 urlpatterns = [
 	url(r'^$', views.index, name='index'),
+	url(r'^new/$', views.add_note, name='add_note'),
 ]
--- whiteboard/views.py
+++ whiteboard/views.py
@@ -1,10 +1,17 @@
 from django.shortcuts import render
+from django.urls import reverse
+from django.contrib import messages
+from django.http import HttpResponseRedirect
+
 from whiteboard.models import Note
+
 import datetime
 
 from django.contrib.auth.decorators import login_required
 
 from users.decorators import has_share
+
+from whiteboard.forms import NewNoteForm
 
 # Create your views here.
 
@@ -19,4 +26,27 @@ def index(request):
 
 	context = {'notes': current_notes}
 	return render(request, 'whiteboard/whiteboard.html', context)
+
+def add_note(request):
+	note_form = NewNoteForm()
+
+	if request.method != 'POST':
+		note_form = NewNoteForm()
+	else:
+		note_form = NewNoteForm(data=request.POST)
+
+		if note_form.is_valid():
+			text = note_form.cleaned_data['text']
+			pub_date = datetime.datetime.today().date()
+			submitted_by = request.user
+			update = Note.objects.create(text=text, pub_date=pub_date, submitted_by=submitted_by)
+
+			messages.add_message(request, messages.SUCCESS, 'Announcement added!')
+			return HttpResponseRedirect(reverse('whiteboard:index'))
+		else:
+			messages.add_message(request, messages.WARNING, 'Something went wrong?')
+
+	context = {'form': note_form}
+	return render(request, 'whiteboard/add_note.html', context)
+
 
