af54305d7dad3c4af9d04d0c910a3eea924347fb
Jakub Szewczyk <kubasz51@gmail.com>
2019-06-24 11:31:37 +0100

    Make leases searchable by first/last names

--- leases/admin.py
+++ leases/admin.py
@@ -17,6 +17,6 @@ class LeaseAdmin(admin.ModelAdmin):
 
 	list_display = list_display + ['member']
 
-	search_fields = ['lease_type', 'start_date', 'end_date',]
+	search_fields = ['lease_type', 'start_date', 'end_date', 'user__first_name', 'user__last_name']
 
 admin.site.register(Lease, LeaseAdmin)
