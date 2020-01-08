2eca4cfb3706e8999003eeceebc658c07bf1e7e0
Jakub Szewczyk <kubasz51@gmail.com>
2020-01-08 21:31:02 +0000

    oops, calculate rent equivalent based on rent rather than expenses

--- finance/qbo.py
+++ finance/qbo.py
@@ -146,7 +146,7 @@ def qbo_profit_loss_report(q: QuickBooks, fc: FinanceConfig, macro: str):
 
     agg_days = (agg['end_date'] - agg['start_date']).days
     agg['rent_avg_value'] = agg['total_income'] * 30.5 / (fc.memberCount * agg_days)
-    agg['rent_eqv_divider'] = agg['rent_avg_value'] / agg['total_expenses']
+    agg['rent_eqv_divider'] = agg['rent_avg_value'] / agg['total_income']
     agg['unused_income'] = agg['total_income'] - agg['total_expenses']
 
     return agg
